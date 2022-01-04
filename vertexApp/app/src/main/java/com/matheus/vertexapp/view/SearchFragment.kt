6fb6f399fd118bbf992110257d549f9fbdcf52ce
Matheus Lima <matheusmt31@gmail.com>
2022-01-04 18:56:11 -0300

    feat: input validation and progress bar for better UX

--- vertexApp/app/src/main/java/com/matheus/vertexapp/view/SearchFragment.kt
+++ vertexApp/app/src/main/java/com/matheus/vertexapp/view/SearchFragment.kt
@@ -29,18 +29,43 @@ class SearchFragment : Fragment() {
         viewModel =  ViewModelProvider(this, VideoViewModelFactory(VideoRespository())).get(VideoViewModel::class.java)
 
         btnSearch.setOnClickListener {
-            viewModel.filterVideos(query = inputSearch.text.toString())
+            if(validateInput(inputSearch.text.toString())){
+                viewModel.filterVideos(query = inputSearch.text.toString())
+                progress_loading.visibility = View.VISIBLE
+                btnSearch.isClickable = false
+                btnSearch.isFocusable = false
+                btnSearch.isEnabled = false
+                btnSearch.isFocusableInTouchMode = false
+            }else{
+                inputSearch.error = "O campo deve possuir pelo menos 3 caracteres"
+            }
+
         }
         viewModel.allVideo.observe(requireActivity(),{
             val direction = SearchFragmentDirections.actionSearchFragmentToResultFragment(it)
             view.findNavController().navigate(direction)
+            enableBtnSearch()
         })
         viewModel.failToGetAllVideos.observe(requireActivity(),{
             Toast.makeText(requireActivity(),it,Toast.LENGTH_SHORT).show()
+            enableBtnSearch()
         })
 
     }
 
+    private fun validateInput(input : String) : Boolean{
+        if(input.isBlank() || input.length < 3 || input.isEmpty() ){
+            return false
+        }
+        return true
+    }
+    private fun enableBtnSearch(){
+        progress_loading.visibility = View.GONE
+        btnSearch.isClickable = true
+        btnSearch.isFocusable = true
+        btnSearch.isEnabled = true
+        btnSearch.isFocusableInTouchMode = true
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
